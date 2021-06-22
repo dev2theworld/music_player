ee82846d4ebecc806ede9b0875a5119fb9b964fd
Michał Wilk <dev2theworld@gmail.com>
2021-06-22 22:01:51 +0200

    JS - basic variables & songs track/text/cover selection added

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const precBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+// Song titles
+const songs = ['Boom Shaka-A-Lak', 'Duloc', 'Monsieur Hood'];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initially load song info DOM
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
