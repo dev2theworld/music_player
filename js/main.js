f25fbf35026f160bbd85843e4692e4379a57f2ac
Michał Wilk <dev2theworld@gmail.com>
2021-06-24 14:05:31 +0200

    JS - progress bar event listener & function updateProgress() added

--- js/main.js
+++ js/main.js
@@ -68,6 +68,12 @@ function nextSong() {
   playSong();
 }
 
+function updateProgress(e) {
+  const {duration, currentTime} = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
 // Event listeners
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
@@ -81,3 +87,5 @@ playBtn.addEventListener('click', () => {
 
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress);
